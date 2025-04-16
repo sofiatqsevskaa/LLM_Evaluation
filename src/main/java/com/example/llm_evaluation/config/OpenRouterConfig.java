b4b2221baef5e7f3ca5a517692c42566cb4f210d
sofiatqsevskaa <tasevska.sofija@yahoo.com>
2025-04-16 11:57:44 +0200

    edited models

--- src/main/java/com/example/llm_evaluation/config/OpenRouterConfig.java
+++ src/main/java/com/example/llm_evaluation/config/OpenRouterConfig.java
@@ -21,7 +21,7 @@ public class OpenRouterConfig {
     @Value("${openrouter.api.url}")
     private String apiUrl;
 
-    private static final List<String> MODELS = Arrays.asList("agentica-org/deepcoder-14b-preview:free", "deepseek/deepseek-chat-v3-0324:free", "google/gemini-2.5-pro-exp-03-25:free");
+    private static final List<String> MODELS = Arrays.asList("agentica-org/deepcoder-14b-preview:free", "deepseek/deepseek-chat-v3-0324:free", "shisa-ai/shisa-v2-llama3.3-70b:free", "qwen/qwq-32b:free");
 
     public String getApiKey() {
         return apiKey;
